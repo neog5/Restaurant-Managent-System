972df34e6256dd63f51a8b8a3fac7eceda1ad38c
neog5 <het.dilippatel20@gmail.com>
2020-12-31 01:15:03 +0530

    minor

--- src/EmployeesV2.java
+++ src/EmployeesV2.java
@@ -341,6 +341,7 @@ public class EmployeesV2 extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
